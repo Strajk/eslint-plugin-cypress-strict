93d432b47f87b1aaef03c92c1d7b809e2cf91d57
Strajk <strajk@me.com>
2019-04-10 17:03:24 +0200

    Fix exporting, don't understand pika

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,5 +1,10 @@
 import unchainedGet from "./unchained-get"
 
-export default {
+const res = {
   "unchained-get": unchainedGet,
 }
+
+// TODO: Make it so this is not needed
+module.exports = res
+
+export default res
